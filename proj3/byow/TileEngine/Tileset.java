3c315b802c4165f8edc5c384a75ff32cc11d35eb
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-04-21 18:12:21 -0700

    Changed floor tile to solid color (reserve dots for bullets)

--- proj3/byow/TileEngine/Tileset.java
+++ proj3/byow/TileEngine/Tileset.java
@@ -17,10 +17,11 @@ import java.awt.Color;
  */
 
 public class Tileset {
+    private static final Color FLOOR_COLOR = new Color(34, 35, 38);
     public static final TETile AVATAR = new TETile('@', Color.white, Color.black, "you");
     public static final TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
             "wall");
-    public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
+    public static final TETile FLOOR = new TETile(' ', Color.black, FLOOR_COLOR,
             "floor");
     public static final TETile NOTHING = new TETile(' ', Color.black, Color.black, "nothing");
     public static final TETile GRASS = new TETile('"', Color.green, Color.black, "grass");
@@ -33,11 +34,11 @@ public class Tileset {
     public static final TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
     public static final TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
     public static final TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
-    public static final TETile PLAYER_NORTH = new TETile('▲', Color.gray, Color.black, "player_N");
-    public static final TETile PLAYER_SOUTH = new TETile('▼', Color.gray, Color.black, "player_S");
-    public static final TETile PLAYER_EAST = new TETile('▶', Color.gray, Color.black, "player_E");
-    public static final TETile PLAYER_WEST = new TETile('◀', Color.gray, Color.black, "player_W");
-    public static final TETile WEAPON_BOX = new TETile('■', new Color(232, 214, 72), Color.black,
+    public static final TETile PLAYER_NORTH = new TETile('▲', Color.gray, FLOOR_COLOR, "player_N");
+    public static final TETile PLAYER_SOUTH = new TETile('▼', Color.gray, FLOOR_COLOR, "player_S");
+    public static final TETile PLAYER_EAST = new TETile('▶', Color.gray, FLOOR_COLOR, "player_E");
+    public static final TETile PLAYER_WEST = new TETile('◀', Color.gray, FLOOR_COLOR, "player_W");
+    public static final TETile WEAPON_BOX = new TETile('■', new Color(232, 214, 72), FLOOR_COLOR,
             "weapon_box");
 }
 
