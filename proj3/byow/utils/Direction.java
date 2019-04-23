7f04e07f1d4782c2aa403862b4ef5c1574b4c09f
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-04-23 16:26:02 -0700

    Formalized shortest-path algorithm in Direction class

--- proj3/byow/utils/Direction.java
+++ proj3/byow/utils/Direction.java
@@ -1,7 +1,17 @@
 package byow.utils;
 
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+import byow.hw4.AStarSolver;
+import byow.hw4.WeightedUndirectedGraph;
+
+import java.util.*;
+
 public enum Direction {
     West, East, North, South, Disoriented;
+
+    static WeightedUndirectedGraph arena;
+    static TETile[][] tiles;
 
     public static Direction parse(char c) {
         switch (c) {
@@ -16,5 +26,52 @@ public enum Direction {
             default:
                 return Disoriented;
         }
+    }
+
+    public static void initPathFinder(TETile[][] tiles, Point s) {
+        Direction.tiles = tiles;
+        Set<Point> visited = new HashSet<>();
+        WeightedUndirectedGraph graph = new WeightedUndirectedGraph();
+        makeGraph(graph, visited, s);
+
+        arena = graph;
+    }
+
+    /**
+     * Load all the points in the map into an A* object for path computation
+     * @param aStarGraph The graph to insert the points
+     * @param visited tracker of whether a vertex is already added
+     * @param s source point
+     */
+
+    private static void makeGraph(WeightedUndirectedGraph aStarGraph,
+                           Set<Point> visited, Point s) {
+        visited.add(s);
+        Set<Point> adjacent = new HashSet<>();
+        //check the four adjacent tiles to see if they are floors
+        if (tiles[s.getX() + 1][s.getY()].equals(Tileset.FLOOR)) {
+            adjacent.add(new Point(s.getX() + 1, s.getY()));
+        }
+        if (tiles[s.getX() - 1][s.getY()].equals(Tileset.FLOOR)) {
+            adjacent.add(new Point(s.getX() - 1, s.getY()));
+        }
+        if (tiles[s.getX()][s.getY() + 1].equals(Tileset.FLOOR)) {
+            adjacent.add(new Point(s.getX(), s.getY() + 1));
+        }
+        if (tiles[s.getX()][s.getY() - 1].equals(Tileset.FLOOR)) {
+            adjacent.add(new Point(s.getX(), s.getY() - 1));
+        }
+        for (Point p : adjacent) {
+            aStarGraph.addEdge(s, p);
+
+            if (!visited.contains(p)) {
+                makeGraph(aStarGraph, visited, p);
+            }
+        }
+    }
+
+    public static List<Point> shortestPath(Point s, Point t) {
+        AStarSolver<Point> solver = new AStarSolver<>(arena, s, t, 1000);
+        return solver.solution();
     }
 }
