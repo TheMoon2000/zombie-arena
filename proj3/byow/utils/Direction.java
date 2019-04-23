e4de2df089b3234f58d299c797b9e5a83089df1c
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-04-22 18:19:22 -0700

    Added a static method that parses the chars 'W', 'A', 'S', and 'D'

--- proj3/byow/utils/Direction.java
+++ proj3/byow/utils/Direction.java
@@ -1,5 +1,20 @@
 package byow.utils;
 
 public enum Direction {
-    West, East, North, South, NorthWest, NorthEast, SouthWest, SouthEast
+    West, East, North, South, Disoriented;
+
+    public static Direction parse(char c) {
+        switch (c) {
+            case 'W':
+                return North;
+            case 'S':
+                return South;
+            case 'D':
+                return East;
+            case 'A':
+                return West;
+            default:
+                return Disoriented;
+        }
+    }
 }
